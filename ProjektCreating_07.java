abad5971b43faf76990fa98f1b60445a5c6e9423
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2022-06-08 09:40:58 +0300

    Add files via upload

---
+++ ProjektCreating_07.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class ProjektCreating_07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String architectName = scanner.nextLine();
+        int numberProjekts = Integer.parseInt(scanner.nextLine());
+    }
+}
