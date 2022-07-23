3756cf2b9a4a399729d774e4de87fa75d3d6ce76
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2022-07-23 17:06:19 +0300

    Create BombNumber_05.java

---
+++ ListExercise/BombNumber_05.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class BombNumber_05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> elements = Arrays.stream(scanner.nextLine()
+                        .split("\\s+"))
+                .collect(Collectors.toList());
+
+        String[] data = scanner.nextLine().split("\\s+");
+        String bombNumber = data[0];
+        int power = Integer.parseInt(data[1]);
+        int sum = 0;
+
+        while (elements.contains(bombNumber)){
+            int elementIndex = elements.indexOf(bombNumber);
+
+            int left = Math.max(0, elementIndex - power);
+            int right = Math.min( elementIndex + power, elements.size() - 1);
+
+            for (int i = right; i >= left; i--) {
+                elements.remove(i);
+
+            }
+        }
+        System.out.println(elements.stream().mapToInt(Integer::parseInt).sum());
+    }
+}
