33228310f127129901fec46eea14514fcc5d2afe
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2022-10-24 09:23:55 +0300

    Create VoinaNumberGame_03.java

---
+++ Java Advanced/SetsAndMapsAdvancedLab/VoinaNumberGame_03.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class VoinaNumberGame_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        LinkedHashSet<Integer> firstDeck = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+
+        LinkedHashSet<Integer> secondDeck = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+
+        int rounds = 50;
+
+        while (rounds-- > 0) {
+
+            int firstCard = firstDeck.iterator().next();
+            firstDeck.remove(firstCard);
+            int secondCard = secondDeck.iterator().next();
+            secondDeck.remove(secondCard);
+
+            if (firstDeck.isEmpty() || secondDeck.isEmpty()) {
+                break;
+            }
+
+            if (firstCard > secondCard) {
+                firstDeck.add(firstCard);
+                firstDeck.add(secondCard);
+            } else if (secondCard > firstCard) {
+                secondDeck.add(firstCard);
+                secondDeck.add(secondCard);
+            }
+
+        }
+        if (firstDeck.size() > secondDeck.size()) {
+            System.out.println("First player win!");
+        } else if (secondDeck.size() > firstDeck.size()) {
+            System.out.println("Second player win!");
+        } else {
+            System.out.println("Draw!");
+        }
+    }
+}
