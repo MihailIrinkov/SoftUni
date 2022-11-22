983cd71eb6d8026cfea58936ec729553daefe25c
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2022-11-22 08:25:34 +0200

    Create FindTheSmallestElement_07.java

---
+++ Java Advanced/FunctionalProgrammingExercise/FindTheSmallestElement_07.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FindTheSmallestElement_07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt).collect(Collectors.toList());
+
+        Function<List<Integer>, Integer> function = list -> {
+            int min = Integer.MAX_VALUE;
+            int index = -1;
+            for (int i = 0; i < list.size(); i++) {
+                int current = list.get(i);
+                if (current <= min) {
+                    min = current;
+                    index = i;
+                }
+            }
+            return index;
+        };
+
+        System.out.println(function.apply(numbers));
+    }
+}
