bcd402f7e212870351ed4cd357e84ddb46bf8623
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2022-12-30 09:03:32 +0200

    GenericBox_01

---
+++ Java Advanced/GenericsExercise/GenericBox_01/Box.java
@@ -0,0 +1,27 @@
+package GenericBox_01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box <T> {
+    private List<T> elements;
+
+    public Box () {
+        this.elements = new ArrayList<>();
+    }
+
+    public void add(T element) {
+        elements.add(element);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        for (T e: elements) {
+            sb.append(e.getClass().getName() + ": " + e + "\n");
+        }
+
+        return sb.toString();
+    }
+}
