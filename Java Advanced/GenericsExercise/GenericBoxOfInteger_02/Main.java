f517e5aa01c0f5e01a4b73c21671f31e918921f3
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2023-01-01 20:44:16 +0200

    GenericBoxOfInteger_02

---
+++ Java Advanced/GenericsExercise/GenericBoxOfInteger_02/Main.java
@@ -0,0 +1,18 @@
+package GenericBoxOfInteger_02;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Box<Integer> box = new Box<>();
+        for (int i = 0; i < n; i++) {
+            box.add(Integer.parseInt(scanner.nextLine()));
+        }
+
+        System.out.println(box);
+    }
+}
