6a6c96a99300ee896371566be4c1089547bce4d0
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2023-02-09 08:14:56 +0200

    StackOfStrings_05

---
+++ Java OOP/InheritanceLab/StackOfStrings_05/StackOfStrings.java
@@ -0,0 +1,29 @@
+package StackOfStrings_05;
+
+import java.util.ArrayList;
+
+public class StackOfStrings {
+    private ArrayList<String> data;
+
+    public StackOfStrings() {
+        this.data = new ArrayList<>();
+    }
+
+    public void push(String s) {
+        this.data.add(s);
+    }
+
+    public String pop() {
+
+        return this.data.remove(this.data.size() - 1);
+    }
+
+    public String peek() {
+        return this.data.get(this.data.size() - 1);
+    }
+
+    public boolean isEmpty() {
+        return this.data.isEmpty();
+    }
+
+}
