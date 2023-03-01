4ab948acddc477cff67b65fa3461cf2bb92c02fb
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2023-03-01 08:16:08 +0200

    MathOperation_01

---
+++ Java OOP/PolymorphismLab/MathOperation_01/MathOperation.java
@@ -0,0 +1,29 @@
+package MathOperation_01;
+
+public class MathOperation {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public static int add(int a, int b, int c, int d) {
+        return a + b + c + d;
+    }
+
+//    public int add(int a, int b) {
+//        return a + b;
+//    }
+//
+//    public int add(int a, int b, int c) {
+//        return a + b + c;
+//    }
+//
+//    public int add(int a, int b, int c, int d) {
+//        return a + b + c + d;
+//    }
+
+}
