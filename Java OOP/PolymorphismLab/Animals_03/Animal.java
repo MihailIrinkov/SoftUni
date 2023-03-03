29f0817d55f5976a75bf879cf534d7856a4db382
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2023-03-03 10:31:58 +0200

    Animals_03

---
+++ Java OOP/PolymorphismLab/Animals_03/Animal.java
@@ -0,0 +1,30 @@
+package Animals_03;
+
+public abstract class Animal {
+    private String name;
+    private String favouriteFood;
+
+    public Animal(String name, String favouriteFood) {
+        this.name = name;
+        this.favouriteFood = favouriteFood;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFavouriteFood() {
+        return favouriteFood;
+    }
+
+    public void setFavouriteFood(String favouriteFood) {
+        this.favouriteFood = favouriteFood;
+    }
+
+    public abstract String explainSelf();
+
+}
