0f5542661cb312601353c7609ea4744e15826ade
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2023-05-29 08:54:54 +0300

    tasks_01_06

---
+++ Java DB/LabBasicCRUD/SelectEmployeeInformation_01.sql
@@ -0,0 +1,10 @@
+SELECT id, first_name, last_name, job_title
+FROM employees
+ORDER BY id;
+
+--SELECT (id),
+--(first_name),
+--(last_name),
+--(job_title)
+--FROM employees
+--ORDER BY id;
---
+++ Java DB/LabBasicCRUD/SelectEmployeesByMultipleFilters_05.sql
@@ -0,0 +1,7 @@
+SELECT
+    *
+FROM
+    employees
+WHERE
+    department_id = 4 AND salary >= 1000
+ORDER BY id;
