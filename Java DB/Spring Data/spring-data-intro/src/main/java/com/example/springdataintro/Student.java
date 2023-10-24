1035179047ca952136964e927aa71f4338a13b7f
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2023-10-24 08:11:02 +0300

    spring-data-intro

---
+++ Java DB/Spring Data/spring-data-intro/src/main/java/com/example/springdataintro/Student.java
@@ -0,0 +1,38 @@
+package com.example.springdataintro;
+
+import jakarta.persistence.*;
+import jdk.jfr.Enabled;
+
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    public Student() {
+    }
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
