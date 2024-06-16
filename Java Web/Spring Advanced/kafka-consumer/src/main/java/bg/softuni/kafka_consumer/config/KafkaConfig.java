f0e3572342d70a2ae8955250205f53df99140f87
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2024-06-16 08:50:56 +0300

    kafka-consumer

---
+++ Java Web/Spring Advanced/kafka-consumer/src/main/java/bg/softuni/kafka_consumer/config/KafkaConfig.java
@@ -0,0 +1,30 @@
+package bg.softuni.kafka_consumer.config;
+
+
+import bg.softuni.kafka_consumer.model.ExchangeRatesDTO;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public ConsumerFactory<String, ExchangeRatesDTO> consumerFactory(KafkaProperties kafkaProperties) {
+        return new DefaultKafkaConsumerFactory<>(kafkaProperties.buildConsumerProperties());
+    }
+
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, ExchangeRatesDTO> kafkaContainerFactory(
+            ConsumerFactory<String, ExchangeRatesDTO> consumerFactory
+    ) {
+        ConcurrentKafkaListenerContainerFactory<String, ExchangeRatesDTO> containerFactory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        containerFactory.setConsumerFactory(consumerFactory);
+        return containerFactory;
+    }
+}
