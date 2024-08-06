8d436d4d199acf70ba74e7578125851e8bdd6454
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2024-08-06 16:53:32 +0300

    test change

--- Java Web/Spring Advanced/15. Individual Project Assignment/06.08.24/ArtGallery/src/test/java/softuni/project/ArtGallery/service/impl/ArtistServiceImplTest.java
+++ Java Web/Spring Advanced/15. Individual Project Assignment/06.08.24/ArtGallery/src/test/java/softuni/project/ArtGallery/service/impl/ArtistServiceImplTest.java
@@ -8,13 +8,16 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 import softuni.project.ArtGallery.exceptions.ArtistNotFoundException;
-import softuni.project.ArtGallery.model.entity.Artist;
+import softuni.project.ArtGallery.model.dto.view.ArtistDetailsViewModel;
+import softuni.project.ArtGallery.model.entity.*;
+import softuni.project.ArtGallery.model.enums.CategoryNames;
+import softuni.project.ArtGallery.model.enums.UserRoles;
 import softuni.project.ArtGallery.repository.ArtistRepository;
 import softuni.project.ArtGallery.repository.CategoryRepository;
 import softuni.project.ArtGallery.service.UserService;
 import softuni.project.ArtGallery.service.helpers.PictureHelperService;
 
-import java.util.Optional;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
@@ -63,8 +66,7 @@ class ArtistServiceImplTest {
     @Test
     void getDetails() {
 
-        Artist artist = new Artist();
-        artist.setName("Rembrand");
+        Artist artist = createTestArtist();
 
         when(mockArtistRepository.findById(1L))
                 .thenReturn(Optional.of(artist));
@@ -72,6 +74,14 @@ class ArtistServiceImplTest {
         Optional<Artist> mockArtistById = mockArtistRepository.findById(1L);
 
         Assertions.assertEquals("Rembrand", mockArtistById.get().getName());
+
+//        Assertions.assertNotEquals(ArtistDetailsViewModel.class,
+//                mockModelMapper.map(artist, ArtistDetailsViewModel.class));
+
+        ArtistDetailsViewModel details = serviceToTest.getDetails(artist.getId());
+//        Assertions.assertNotNull(details);
+
+        Assertions.assertEquals(artist.getName(), details.getName());
     }
 
     @Test
@@ -86,5 +96,31 @@ class ArtistServiceImplTest {
 
     @Test
     void getAllByCategory() {
+
+    }
+
+    private static Artist createTestArtist() {
+
+        User user = new User();
+        user.setUsername("test");
+        user.setPassword("12345");
+        user.setRoles(Set.of(new Role(UserRoles.ADMIN),
+                new Role(UserRoles.USER)));
+        user.setAge(25);
+        user.setEmail("test@test.com");
+        user.setFullName("fullName");
+        user.setId(100L);
+
+        Artist artist = new Artist();
+        artist.setId(1L);
+        artist.setName("Rembrand");
+        artist.setAuthor(user);
+        artist.setImageUrl("testartis2/30d7cc37-420a-4757-a672-14cfdc6ede03.jpg");
+        artist.setVideoUrl("uRBngj5z4oY");
+        artist.setCategories(Set.of(new Category(CategoryNames.TEMPERA)));
+        artist.setComments(new ArrayList<>());
+        artist.setDescription("mockDescription");
+        artist.setPictures(new ArrayList<>());
+        return artist;
     }
 }
