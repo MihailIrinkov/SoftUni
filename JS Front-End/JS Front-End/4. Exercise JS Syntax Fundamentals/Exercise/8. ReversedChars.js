965696b39535ce4376e06bca2f2a907f5fb9c00c
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2024-11-03 16:10:10 +0200

    Exercise

---
+++ JS Front-End/JS Front-End/4. Exercise JS Syntax Fundamentals/Exercise/8. ReversedChars.js
@@ -0,0 +1,22 @@
+function solve(char1, char2, char3) {
+    // let input = '';
+    // input = char1 + char2 + char3;
+    // let output = '';
+    //
+    // for (let i = input.length; i >= 0 ; i--) {
+    //     output += String(input[i]) + ' ';
+    // }
+    // console.log(String(output.trim()));
+
+    console.log(char3 + ' ' + char2 + ' ' + char1)
+}
+
+solve('A',
+    'B',
+    'C'
+);
+
+solve('1',
+    'L',
+    '&'
+);
