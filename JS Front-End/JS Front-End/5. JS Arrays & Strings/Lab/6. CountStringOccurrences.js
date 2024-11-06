ac88bb930054348ed7c0ee3630add713447fb554
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2024-11-06 16:06:06 +0200

    Lab

--- JS Front-End/JS Front-End/5. JS Arrays & Strings/Lab/6. CountStringOccurrences.js
+++ JS Front-End/JS Front-End/5. JS Arrays & Strings/Lab/6. CountStringOccurrences.js
@@ -1,8 +1,12 @@
-function solve(text, word) {
-    const pattern = new RegExp(`\\b${word}\\b`, 'g');
-    const result = text.match(pattern) || [];
+// function solve(text, word) {
+//     const pattern = new RegExp(`\\b${word}\\b`, 'g');
+//     const result = text.match(pattern) || [];
+//
+//     console.log(result.length);
+// }
 
-    console.log(result.length);
+function solve(text, word) {
+    console.log(text.split(' ').filter(w => (w === word)).length);
 }
 
 solve('This is a word and it also is a sentence',
