1c09e6bafd2d7dde7164b89126803ccfd829aff8
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2024-11-06 11:16:22 +0200

    Lab

--- JS Front-End/JS Front-End/5. JS Arrays & Strings/Lab/1. SumFirstAndLastArrayElements.js
+++ JS Front-End/JS Front-End/5. JS Arrays & Strings/Lab/1. SumFirstAndLastArrayElements.js
@@ -1,5 +1,11 @@
+// function solve(input) {
+//     let sum = input[0] + input[input.length - 1];
+//
+//     console.log(sum);
+// }
+
 function solve(input) {
-    let sum = input[0] + input[input.length - 1];
+    let sum = input.shift() + input.pop();
 
     console.log(sum);
 }
