427c1fa7060532e69505eba068ad6127044d9c5f
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2024-11-10 11:16:04 +0200

    Exercise

--- JS Front-End/JS Front-End/6. Exercise JS Arrays & Strings/JS Arrays and Strings – More Exercises/2. BitcoinMining.js
+++ JS Front-End/JS Front-End/6. Exercise JS Arrays & Strings/JS Arrays and Strings – More Exercises/2. BitcoinMining.js
@@ -1,1 +1,32 @@
-function solve(){}
+function solve(input){
+
+    const bitcoinPrice = 11949.16;
+    const goldPrice = 67.51;
+
+    let bitcoinCount = 0;
+    let bitcoinDayIndex = 0;
+
+    const currencyCount = input.reduce(function (earnings, dailyGold, i){
+        const currentDay = i + 1;
+        let bitcoinDayCount = 0;
+
+        if (currentDay % 3 == 0) dailyGold -= dailyGold * 0.3;
+
+        earnings += dailyGold * goldPrice;
+
+        bitcoinDayCount = Math.floor(earnings / bitcoinPrice);
+
+        if (bitcoinDayCount) {
+            if (!bitcoinDayIndex) bitcoinDayIndex = currentDay;
+            earnings -= (bitcoinDayCount * bitcoinPrice);
+            bitcoinCount += bitcoinDayCount;
+        }
+        return earnings;
+    }, 0);
+
+    console.log(`Bought bitcoins: ${bitcoinCount}`);
+    if ( bitcoinDayIndex ) console.log(`Day of the first purchased bitcoin: ${ bitcoinDayIndex }`);
+    console.log(`Left money: ${currencyCount.toFixed(2)} lv.`);
+}
+
+solve([100, 200, 300]);
