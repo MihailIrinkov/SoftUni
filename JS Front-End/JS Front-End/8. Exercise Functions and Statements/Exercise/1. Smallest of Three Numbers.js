2fc386e365838f75a7b5f9dc0e5859620a6b4e1a
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2024-11-12 09:29:29 +0200

    Exercise

---
+++ JS Front-End/JS Front-End/8. Exercise Functions and Statements/Exercise/1. Smallest of Three Numbers.js
@@ -0,0 +1,18 @@
+function smallestNumber(num1, num2, num3){
+    return Math.min(...arguments);
+}
+
+console.log(smallestNumber(2,
+    5,
+    3
+));
+console.log(smallestNumber(600,
+    342,
+    123
+));
+console.log(smallestNumber(25,
+    21,
+    4
+));
+
+
