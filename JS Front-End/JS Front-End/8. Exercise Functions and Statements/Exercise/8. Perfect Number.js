7aed249ba95a08736c8aad41870a94e11e1d5c30
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2024-11-13 09:04:38 +0200

    Exercise

---
+++ JS Front-End/JS Front-End/8. Exercise Functions and Statements/Exercise/8. Perfect Number.js
@@ -0,0 +1,18 @@
+function perfectNumber(num) {
+
+    let sum = 1;
+
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            sum += i;
+        }
+    }
+
+    if (sum === num) {
+        console.log('We have a perfect number!');
+    } else {
+        console.log('It\'s not so perfect.');
+    }
+}
+
+perfectNumber(6);
