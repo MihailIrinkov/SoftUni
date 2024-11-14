472ff91e3e5e814ac9ad91dc80c95ac130d60b7f
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2024-11-14 08:11:45 +0200

    Lab

---
+++ JS Front-End/JS Front-End/9. Object and Classes/Lab/1. PersonInfo.js
@@ -0,0 +1,15 @@
+function personInfo(firstName, lastName, age) {
+
+    let person = {
+        firstName: firstName,
+        lastName: lastName,
+        age: age,
+    }
+
+    return person;
+}
+
+console.log(personInfo("Peter",
+    "Pan",
+    "20"
+));
