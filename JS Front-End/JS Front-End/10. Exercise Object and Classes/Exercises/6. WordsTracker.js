78c006a265efe1775ebb1e30391b25e4329300be
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2024-11-19 12:28:00 +0200

    Exercise

---
+++ JS Front-End/JS Front-End/10. Exercise Object and Classes/Exercises/6. WordsTracker.js
@@ -0,0 +1,21 @@
+function solve(input) {
+
+    const searchWords = input.shift().split(' ');
+    const matched = searchWords.reduce((matched, word) => ({...matched, [word]: 0}), {});
+
+    input.forEach(word => {
+        if (matched.hasOwnProperty(word)) matched[word] += 1;
+    });
+
+    Object
+        .entries(matched)
+        .sort((a, b) => b[1] - a[1])
+        .forEach(([word, count]) => console.log((`${word} - ${count}`)));
+
+}
+
+solve([
+        'this sentence',
+        'In', 'this', 'sentence', 'you', 'have', 'to', 'count', 'the', 'occurrences', 'of', 'the', 'words', 'this', 'and', 'sentence', 'because', 'this', 'is', 'your', 'task'
+    ]
+);
