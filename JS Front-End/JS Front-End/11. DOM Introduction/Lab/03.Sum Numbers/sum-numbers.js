3317ce1deb97d49615ab6dc5dc009e09f1017208
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2024-11-21 11:20:04 +0200

    Lab

---
+++ JS Front-End/JS Front-End/11. DOM Introduction/Lab/03.Sum Numbers/sum-numbers.js
@@ -0,0 +1,9 @@
+function calc() {
+    
+    const num1Element = document.querySelector('#num1');
+    const num2Element = document.querySelector('#num2');
+    const sumElement = document.querySelector('#sum');
+
+    sumElement.value = Number(num1Element.value) + Number(num2Element.value);
+}
+
