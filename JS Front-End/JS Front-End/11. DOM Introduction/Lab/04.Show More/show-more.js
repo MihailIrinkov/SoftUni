78665a9133529d306a483d0691452d56de47769f
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2024-11-21 11:40:27 +0200

    Lab

---
+++ JS Front-End/JS Front-End/11. DOM Introduction/Lab/04.Show More/show-more.js
@@ -0,0 +1,8 @@
+function showText() {
+    
+    const textElement = document.querySelector('#text');
+    const linkElement = document.querySelector('#more');
+
+    textElement.style.display = 'inline';
+    linkElement.style.display = 'none';
+}
