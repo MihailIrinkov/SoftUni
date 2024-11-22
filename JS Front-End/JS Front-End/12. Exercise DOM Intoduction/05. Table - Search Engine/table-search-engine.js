f3812a6e4607ac88999ca67b09fe05257a28a35d
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2024-11-22 09:58:54 +0200

    Exercise

---
+++ JS Front-End/JS Front-End/12. Exercise DOM Intoduction/05. Table - Search Engine/table-search-engine.js
@@ -0,0 +1,17 @@
+function solve() {
+   
+   const searchStr = document.querySelector('#searchField').value.toLowerCase().trim();
+   const students = document.querySelectorAll('table tbody tr');
+
+   if (searchStr == '') return;
+
+   students.forEach(student => {
+
+      student.classList.remove('select');
+
+      if (student.textContent.toLowerCase().includes(searchStr)) {
+         student.classList.add('select');
+      }
+   });
+
+}
