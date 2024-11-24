72220e6ac0767395c2e7ca98447603f39230e7df
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2024-11-24 12:41:04 +0200

    Lab

--- JS Front-End/JS Front-End/13. DOM Manipulation and Events/02. Delete-From-Table/delete-from-table.js
+++ JS Front-End/JS Front-End/13. DOM Manipulation and Events/02. Delete-From-Table/delete-from-table.js
@@ -1,19 +1,22 @@
 function deleteByEmail() {
     
-    const inputEl = document.querySelector('input[name="email"]').value.toLowerCase();
+    const inputEl = document.querySelector('input[name="email"]');
+    const searchStr = inputEl.value.toLowerCase();
     const persons = document.querySelectorAll('table tbody tr td:nth-child(2)');
     const resultEl = document.querySelector('#result');
 
-    if (inputEl == '') return;
+    if (searchStr.trim() == '') return;
 
-    persons.forEach(person => {
+    const [machedEl] = [...persons]
+                        .filter(person => person.textContent.toLowerCase()
+                        .includes(searchStr));
+                        
+    if (machedEl) {
+        machedEl.parentElement.remove();
+        resultEl.textContent = 'Deleted.';
+    } else {
+        resultEl.textContent = 'Not found.';
+    }                    
 
-        if (person.textContent.toLowerCase().includes(inputEl)) {
-            person.parentElement.remove();
-            resultEl.textContent = 'Deleted.'
-        } else {
-            resultEl.textContent = 'Not found.';
-        }
-        
-    });
+    inputEl.value = '';
 }
