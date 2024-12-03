b7659e1e9d01c37be94867ed1dc5423c13bdcbc7
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2024-12-03 19:39:59 +0200

    Exercise

--- JS Front-End/JS Front-End/ExamPrep13April2024/02.Contact-List_Resources/app.js
+++ JS Front-End/JS Front-End/ExamPrep13April2024/02.Contact-List_Resources/app.js
@@ -1,76 +1,84 @@
 window.addEventListener("load", solve);
 
 function solve() {
-    const nameEl = document.querySelector('#name');
-    const phoneNumberEl  = document.querySelector('#phone');
-    const categoryEl = document.querySelector('#category');
+  const nameEl = document.querySelector('#name');
+  const phoneNumberEl = document.querySelector('#phone');
+  const categoryEl = document.querySelector('#category');
 
-    const ulEl = document.querySelector('#check-list');
-    // const contactList = document
+  const ulEl = document.querySelector('#check-list');
+  const contactListUl = document.querySelector('#contact-list');
 
-    const addButton = document.querySelector('#add-btn');
+  const addButton = document.querySelector('#add-btn');
 
 
-    if (nameEl == '' || phoneNumberEl == '' || categoryEl == '') return;
+  if (nameEl == '' || phoneNumberEl == '' || categoryEl == '') return;
 
-    // console.log(nameEl);
+  addButton.addEventListener('click', () => {
+    createData(nameEl.value, phoneNumberEl.value, categoryEl.value);
 
-    addButton.addEventListener('click', () => {
-      createData(nameEl.value, phoneNumberEl.value, categoryEl.value);
+    clear();
+  });
 
-      clear();
+  function createData(name, phone, category) {
+    const articleEl = document.createElement('article');
+
+    const namePEl = document.createElement('p');
+    namePEl.textContent = `name:${name}`;
+    articleEl.appendChild(namePEl);
+
+    const phonePEl = document.createElement('p');
+    phonePEl.textContent = `phone:${phone}`;
+    articleEl.appendChild(phonePEl);
+
+    const categoryPEl = document.createElement('p');
+    categoryPEl.textContent = `category:${category}`;
+    articleEl.appendChild(categoryPEl);
+
+    const liEl = document.createElement('li');
+    liEl.appendChild(articleEl);
+
+    ulEl.appendChild(liEl);
+
+    const divEl = document.createElement('div');
+    divEl.classList.add('buttons');
+
+    const editBtn = document.createElement('button');
+    editBtn.classList.add('edit-btn');
+    divEl.appendChild(editBtn);
+
+    const saveBtn = document.createElement('button');
+    saveBtn.classList.add('save-btn');
+    divEl.appendChild(saveBtn);
+
+    liEl.appendChild(divEl);
+
+    const editBtnEl = document.querySelector('.edit-btn');
+    editBtnEl.addEventListener('click', () => {
+      nameEl.value = name;
+      phoneNumberEl.value = phone;
+      categoryEl.value = category;
+
+      liEl.remove();
     });
 
-    function createData(name, phone, category) {
-      const articleEl = document.createElement('article');
+    saveBtn.addEventListener('click', () => {
+      contactListUl.appendChild(liEl);
+      divEl.remove();
 
-      const namePEl = document.createElement('p');
-      namePEl.textContent = `name:${name}`;
-      articleEl.appendChild(namePEl);
+      const deleteButton = document.createElement('button');
+      deleteButton.classList.add('del-btn');
+      liEl.appendChild(deleteButton);
 
-      const phonePEl = document.createElement('p');
-      phonePEl.textContent = `phone:${phone}`;
-      articleEl.appendChild(phonePEl);
-
-      const categoryPEl = document.createElement('p');
-      categoryPEl.textContent = `category:${category}`;
-      articleEl.appendChild(categoryPEl);
-
-      const liEl = document.createElement('li');
-      liEl.appendChild(articleEl);
-
-      // const ulEl = document.querySelector('#check-list');
-      ulEl.appendChild(liEl);
-
-      const divEl = document.createElement('div');
-      divEl.classList.add('buttons');
-
-      const editBtn = document.createElement('button');
-      editBtn.classList.add('edit-btn');
-      divEl.appendChild(editBtn);
-
-      const saveBtn = document.createElement('button');
-      saveBtn.classList.add('save-btn');
-      divEl.appendChild(saveBtn);
-
-      liEl.appendChild(divEl);
-
-      const editBtnEl = document.querySelector('.edit-btn');
-      editBtnEl.addEventListener('click', () => {
-        nameEl.value = name;
-        phoneNumberEl.value = phone;
-        categoryEl.value = category;
-
-        liEl.remove(); 
+      deleteButton.addEventListener('click', (e) => {
+        e.target.parentElement.remove();
       });
-    }
+    });
 
-    // console.log(createData(nameEl, phoneNumberEl, categoryEl));
-
-    function clear() {
-      nameEl.value = '';
-      phoneNumberEl.value = '';
-      categoryEl.value = '';
-    }
   }
-  
+
+  function clear() {
+    nameEl.value = '';
+    phoneNumberEl.value = '';
+    categoryEl.value = '';
+  }
+}
