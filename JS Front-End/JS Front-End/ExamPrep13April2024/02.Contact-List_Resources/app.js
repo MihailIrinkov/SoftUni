0808a7bcf4441820ace26dca5d6c353b7342269c
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2024-12-03 16:50:26 +0200

    Exercise

---
+++ JS Front-End/JS Front-End/ExamPrep13April2024/02.Contact-List_Resources/app.js
@@ -0,0 +1,76 @@
+window.addEventListener("load", solve);
+
+function solve() {
+    const nameEl = document.querySelector('#name');
+    const phoneNumberEl  = document.querySelector('#phone');
+    const categoryEl = document.querySelector('#category');
+
+    const ulEl = document.querySelector('#check-list');
+    // const contactList = document
+
+    const addButton = document.querySelector('#add-btn');
+
+
+    if (nameEl == '' || phoneNumberEl == '' || categoryEl == '') return;
+
+    // console.log(nameEl);
+
+    addButton.addEventListener('click', () => {
+      createData(nameEl.value, phoneNumberEl.value, categoryEl.value);
+
+      clear();
+    });
+
+    function createData(name, phone, category) {
+      const articleEl = document.createElement('article');
+
+      const namePEl = document.createElement('p');
+      namePEl.textContent = `name:${name}`;
+      articleEl.appendChild(namePEl);
+
+      const phonePEl = document.createElement('p');
+      phonePEl.textContent = `phone:${phone}`;
+      articleEl.appendChild(phonePEl);
+
+      const categoryPEl = document.createElement('p');
+      categoryPEl.textContent = `category:${category}`;
+      articleEl.appendChild(categoryPEl);
+
+      const liEl = document.createElement('li');
+      liEl.appendChild(articleEl);
+
+      // const ulEl = document.querySelector('#check-list');
+      ulEl.appendChild(liEl);
+
+      const divEl = document.createElement('div');
+      divEl.classList.add('buttons');
+
+      const editBtn = document.createElement('button');
+      editBtn.classList.add('edit-btn');
+      divEl.appendChild(editBtn);
+
+      const saveBtn = document.createElement('button');
+      saveBtn.classList.add('save-btn');
+      divEl.appendChild(saveBtn);
+
+      liEl.appendChild(divEl);
+
+      const editBtnEl = document.querySelector('.edit-btn');
+      editBtnEl.addEventListener('click', () => {
+        nameEl.value = name;
+        phoneNumberEl.value = phone;
+        categoryEl.value = category;
+
+        liEl.remove(); 
+      });
+    }
+
+    // console.log(createData(nameEl, phoneNumberEl, categoryEl));
+
+    function clear() {
+      nameEl.value = '';
+      phoneNumberEl.value = '';
+      categoryEl.value = '';
+    }
+  }
+  
