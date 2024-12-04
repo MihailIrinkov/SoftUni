c7325489e8e396d601e8cbe8a2e6052cdfbcc589
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2024-12-04 22:56:39 +0200

    Exercise

--- JS Front-End/JS Front-End/ExamPrep_2_13April2024/01.Spellbook Unraveling/app.js
+++ JS Front-End/JS Front-End/ExamPrep_2_13April2024/01.Spellbook Unraveling/app.js
@@ -1,7 +1,6 @@
 function solve(commands) {
 
     let spell = commands.shift();
-    let encodedSpell = '';
 
     let command = commands.shift();
 
@@ -59,3 +58,60 @@ solve((["hZwemtroiui5tfone1haGnanbvcaploL2u2a2n2i2m",
     "Reverse!sad",
     "End"])
 );
+
+
+
+// function solve(commands) {
+
+//     let spell = commands.shift();
+//     let encodedSpell = '';
+
+//     let command = commands.shift();
+
+//     while (command !== 'End') {
+
+//         let action = command.split('!')[0];
+
+//         switch (action) {
+
+//             case 'RemoveEven':
+//                 let spellToEncode = spell.split('');
+//                 for (let i = 0; i < spellToEncode.length; i++) {
+//                     if (i % 2 === 0) {
+//                         encodedSpell += spellToEncode[i];
+//                         spell = encodedSpell;
+        
+//                     }
+//                 }
+//                 encodedSpell = '';
+//                 console.log(spell);
+//                 break;
+
+//             case 'TakePart':
+//                 let firstIndex = Number(command.split('!')[1]);
+//                 let secondIndex = Number(command.split('!')[2]);
+
+//                 if (firstIndex > 0 || secondIndex > 0) {
+//                     spell = spell.slice(firstIndex, secondIndex);
+//                 }
+//                 console.log(spell);
+//                 break;
+
+//             case 'Reverse':
+//                 let combination = command.split('!')[1];
+//                 if (spell.includes(combination)) {
+//                     let reverseString = combination.split('').reverse().join('');
+
+//                     spell = spell.replace(combination, '');
+//                     spell += reverseString;
+//                     console.log(spell);
+//                 } else {
+//                     console.log('Error');
+//                 }
+//                 break;
+//         }
+//         command = commands.shift();
+//     }
+
+//     console.log(`The concealed spell is: ${spell}`);
+// }
