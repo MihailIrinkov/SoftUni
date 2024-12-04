8ea3f02a5525ae4bfeab088b7c7bdf8246f7e8ef
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2024-12-04 20:35:53 +0200

    Exercise

---
+++ JS Front-End/JS Front-End/ExamPrep_2_13April2024/01.Spellbook Unraveling/app.js
@@ -0,0 +1,65 @@
+function solve(commands) {
+
+    let spell = commands.shift();
+    let encodedSpell = '';
+
+    let command = commands.shift();
+
+    while (command !== 'End') {
+
+        let action = command.split('!')[0];
+
+        switch (action) {
+
+            case 'RemoveEven':
+                let spellToEncode = spell.split('');
+                for (let i = 0; i < spellToEncode.length; i++) {
+                    if (i % 2 === 0) {
+                        encodedSpell += spellToEncode[i];
+                        spell = encodedSpell;
+                    }
+                }
+                console.log(spell);
+                break;
+
+            case 'TakePart':
+                let firstIndex = Number(command.split('!')[1]);
+                let secondIndex = Number(command.split('!')[2]);
+
+                spell = spell.slice(firstIndex, secondIndex);
+                console.log(spell);
+                break;
+
+            case 'Reverse':
+                let combination = command.split('!')[1];
+                if (spell.includes(combination)) {
+
+                    let reverseString = combination.split('').reverse().join('');
+
+                    spell = spell.replace(combination, '').concat(reverseString);
+                    console.log(spell);
+                } else {
+                    console.log('Error');
+                }
+                break;
+        }
+        command = commands.shift();
+    }
+
+    console.log(`The concealed spell is: ${spell}`.trim());
+}
+
+solve((["asAsl2adkda2mdaczsa",
+    "RemoveEven",
+    "TakePart!1!9",
+    "Reverse!maz",
+    "End"])
+);
+
+solve((["hZwemtroiui5tfone1haGnanbvcaploL2u2a2n2i2m",
+    "TakePart!31!42",
+    "RemoveEven",
+    "Reverse!anim",
+    "Reverse!sad",
+    "End"])
+);
