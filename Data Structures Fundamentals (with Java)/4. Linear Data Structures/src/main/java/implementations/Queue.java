f26fa6166b4a883329475972213c7cd076cdb886
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2025-02-19 08:32:03 +0200

    3. Queue

--- Data Structures Fundamentals (with Java)/4. Linear Data Structures/src/main/java/implementations/Queue.java
+++ Data Structures Fundamentals (with Java)/4. Linear Data Structures/src/main/java/implementations/Queue.java
@@ -5,34 +5,93 @@ import interfaces.AbstractQueue;
 import java.util.Iterator;
 
 public class Queue<E> implements AbstractQueue<E> {
+    private Node<E> head;
+    private int size;
+
+    public Queue() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    private static class Node<E> {
+        private E value;
+        private Node<E> next;
+
+        Node(E element) {
+            this.value = element;
+        }
+    }
 
     @Override
     public void offer(E element) {
+        Node<E> toInsert = new Node<>(element);
+        if (this.isEmpty()) {
+            this.head = toInsert;
+        } else {
 
+            Node<E> current = this.head;
+
+            while (current.next != null) {
+                current = current.next;
+            }
+
+            current.next = toInsert;
+        }
+        this.size++;
     }
 
     @Override
     public E poll() {
-        return null;
+        ensureNonEmpty();
+
+        Node<E> first = this.head;
+        this.head = first.next;
+
+        this.size--;
+
+        return first.value;
     }
 
     @Override
     public E peek() {
-        return null;
+        ensureNonEmpty();
+
+        return this.head.value;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.size == 0;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+            private Node<E> current = head;
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public E next() {
+                E value = current.value;
+
+                current = current.next;
+
+                return value;
+            }
+        };
+    }
+
+    private void ensureNonEmpty() {
+        if (this.isEmpty()) {
+            throw new IllegalStateException();
+        }
     }
 }
