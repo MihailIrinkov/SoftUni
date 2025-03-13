85a5ebbef206f7eed85d9442302936854992d90b
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2025-03-13 11:40:04 +0200

    3. PriorityQueue

--- Data Structures Fundamentals (with Java)/8. Heaps, BST/java/implementations/MaxHeap.java
+++ Data Structures Fundamentals (with Java)/8. Heaps, BST/java/implementations/MaxHeap.java
@@ -7,11 +7,16 @@ import java.util.Collections;
 import java.util.List;
 
 public class MaxHeap<E extends Comparable<E>> implements Heap<E> {
-    private List<Integer> elements;
+    private List<E> elements;
+
+    public MaxHeap() {
+        this.elements = new ArrayList<>();
+    }
 
     @Override
     public void add(E element) {
-
+        this.elements.add(element);
+        this.heapifyUp(this.size() - 1);
     }
 
     @Override
@@ -21,7 +26,14 @@ public class MaxHeap<E extends Comparable<E>> implements Heap<E> {
 
 
     private void heapifyUp(int index) {
+        while (index > 0 && isLess(getParentAt(index), index)) {
+            Collections.swap(this.elements, getParentAt(index), index);
+            index = getParentAt(index);
+        }
+    }
 
+    private boolean isLess(int childIndex, int parentIndex) {
+        return getAt(childIndex).compareTo(getAt(parentIndex)) < 0;
     }
 
     private boolean hasParent(int index) {
@@ -42,7 +54,13 @@ public class MaxHeap<E extends Comparable<E>> implements Heap<E> {
 
     @Override
     public E peek() {
+        if (this.size() == 0) {
+            throw new IllegalStateException("Illegal call to peek on empty heap!");
+        }
+        return getAt(0);
+    }
 
-        return null;
+    private E getAt(int index) {
+        return this.elements.get(index);
     }
 }
--- Data Structures Fundamentals (with Java)/8. Heaps, BST/java/implementations/PriorityQueue.java
+++ Data Structures Fundamentals (with Java)/8. Heaps, BST/java/implementations/PriorityQueue.java
@@ -8,18 +8,37 @@ import java.util.List;
 
 public class PriorityQueue<E extends Comparable<E>> implements AbstractQueue<E> {
 
-    @Override
-    public int size() {
-        return 0;
+    private List<E> elements;
+
+    public PriorityQueue() {
+        this.elements = new ArrayList<>();
     }
 
     @Override
     public void add(E element) {
-
+        this.elements.add(element);
+        this.heapifyUp(this.size() - 1);
     }
 
-    private void heapifyUp(int index) {
+    @Override
+    public int size() {
+        return this.elements.size();
+    }
 
+
+    private void heapifyUp(int index) {
+        while (index > 0 && isLess(getParentAt(index), index)) {
+            Collections.swap(this.elements, getParentAt(index), index);
+            index = getParentAt(index);
+        }
+    }
+
+    private boolean isLess(int childIndex, int parentIndex) {
+        return getAt(childIndex).compareTo(getAt(parentIndex)) < 0;
+    }
+
+    private boolean hasParent(int index) {
+        return false;
     }
 
     private int getParentAt(int index) {
@@ -36,20 +55,25 @@ public class PriorityQueue<E extends Comparable<E>> implements AbstractQueue<E> 
 
     @Override
     public E peek() {
-        return null;
+        ensureNonEmpty();
+        return getAt(0);
     }
 
     private void ensureNonEmpty() {
-
+        if (this.size() == 0) {
+            throw new IllegalStateException("Illegal call to peek on empty heap!");
+        }
     }
 
     @Override
     public E poll() {
+        ensureNonEmpty();
+        E returneValaue = getAt(0);
 
-        return null;
+        return returneValaue;
     }
 
-    private void heapifyDown(int index) {
-
+    private E getAt(int index) {
+        return this.elements.get(index);
     }
 }
