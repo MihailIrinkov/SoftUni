c33082f00f74f23ba306ee7005ccee16c4318a30
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2025-03-18 11:40:54 +0200

    4. Binary Search Tree (BST)

--- Data Structures Fundamentals (with Java)/8. Heaps, BST/java/implementations/BinarySearchTree.java
+++ Data Structures Fundamentals (with Java)/8. Heaps, BST/java/implementations/BinarySearchTree.java
@@ -3,41 +3,118 @@ package implementations;
 import interfaces.AbstractBinarySearchTree;
 
 public class BinarySearchTree<E extends Comparable<E>> implements AbstractBinarySearchTree<E> {
+    private Node<E> root;
+    private Node<E> leftChild;
+    private Node<E> rightChild;
+
+    public BinarySearchTree() {
+
+    }
+
+    public BinarySearchTree(Node<E> root) {
+        this.copy(root);
+    }
+
+    private void copy(Node<E> node) {
+        if (node != null) {
+            this.insert(node.value);
+            this.copy(node.leftChild);
+            this.copy(node.rightChild);
+        }
+    }
 
     @Override
     public void insert(E element) {
+        Node<E> newNode = new Node<>(element);
+        if (this.getRoot() == null) {
+            this.root = newNode;
+        } else {
+            Node<E> current = this.root;
+            Node<E> previous = current;
 
+            while (current != null) {
+                previous = current;
+                if (isLess(element, current.value)) {
+                    current = current.leftChild;
+                } else if (isGreater(element, current.value)) {
+                    current = current.rightChild;
+                } else if (areEqual(element, current.value)) {
+                    return;
+                }
+            }
+
+            if (isLess(element, previous.value)) {
+                previous.leftChild = newNode;
+            } else if (isGreater(element, previous.value)) {
+                previous.rightChild = newNode;
+            }
+        }
+    }
+
+    private boolean isLess(E first, E second) {
+        return first.compareTo(second) < 0;
+    }
+
+    private boolean isGreater(E first, E second) {
+        return first.compareTo(second) > 0;
+    }
+
+    private boolean areEqual(E first, E second) {
+        return first.compareTo(second) == 0;
     }
 
     @Override
     public boolean contains(E element) {
+        Node<E> current = this.root;
 
+        while (current != null) {
+            if (isLess(element, current.value)) {
+                current = current.leftChild;
+            } else if (isGreater(element, current.value)) {
+                current = current.rightChild;
+            } else if (areEqual(element, current.value)) {
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public AbstractBinarySearchTree<E> search(E element) {
+        AbstractBinarySearchTree<E> result = new BinarySearchTree<>();
 
-        return null;
+        Node<E> current = this.root;
+
+        while (current != null) {
+            if (isLess(element, current.value)) {
+                current = current.leftChild;
+            } else if (isGreater(element, current.value)) {
+                current = current.rightChild;
+            } else if (areEqual(element, current.value)) {
+                return new BinarySearchTree<>(current);
+            }
+        }
+
+        return result;
     }
 
     @Override
     public Node<E> getRoot() {
-        return null;
+        return this.root;
     }
 
     @Override
     public Node<E> getLeft() {
-        return null;
+        return this.leftChild;
     }
 
     @Override
     public Node<E> getRight() {
-        return null;
+        return this.rightChild;
     }
 
     @Override
     public E getValue() {
-        return null;
+        return this.root.value;
     }
 }
