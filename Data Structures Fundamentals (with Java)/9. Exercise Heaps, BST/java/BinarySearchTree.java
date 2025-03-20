79e2065f0acd074274a3bea5d49cfb304b7d128c
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2025-03-20 16:43:39 +0200

    1. BST Operations

--- Data Structures Fundamentals (with Java)/9. Exercise Heaps, BST/java/BinarySearchTree.java
+++ Data Structures Fundamentals (with Java)/9. Exercise Heaps, BST/java/BinarySearchTree.java
@@ -16,7 +16,7 @@ public class BinarySearchTree<E extends Comparable<E>> {
         private Node<E> leftChild;
         private Node<E> rightChild;
 
-		public Node(E value) {
+        public Node(E value) {
             this.value = value;
         }
 
@@ -32,9 +32,19 @@ public class BinarySearchTree<E extends Comparable<E>> {
             return this.value;
         }
     }
-	
-	public void eachInOrder(Consumer<E> consumer) {
-            
+
+    public void eachInOrder(Consumer<E> consumer) {
+        nodeInOrder(this.root, consumer);
+    }
+
+    private void nodeInOrder(Node<E> node, Consumer<E> consumer) {
+        if (node == null) {
+            return;
+        }
+
+        nodeInOrder(node.getLeft(), consumer);
+        consumer.accept(node.getValue());
+        nodeInOrder(node.getRight(), consumer);
     }
 
     public Node<E> getRoot() {
@@ -55,25 +65,56 @@ public class BinarySearchTree<E extends Comparable<E>> {
         } else if (isLess(element, node)) {
             if (node.getLeft() == null) {
                 node.leftChild = new Node<>(element);
+            } else {
+                insertInto(node.getLeft(), element);
             }
-        } else {
-            insertInto(node.getLeft(), element);
         }
     }
 
     public boolean contains(E element) {
-        return false;
+        return containsNode(this.root, element);
+
+//        Node<E> current = this.root;
+//
+//        while (current != null) {
+//            if (isEqual(element, current)) {
+//                break;
+//            } else if (isGreater(element, current)) {
+//                current = current.getRight();
+//            } else {
+//                current = current.getLeft();
+//            }
+//        }
+//
+//        return current != null;
+    }
+
+    private boolean containsNode(Node<E> node, E element) {
+        if (node == null) {
+            return false;
+        }
+
+        if (isEqual(element, node)) {
+            return true;
+        } else if (isGreater(element, node)) {
+            return containsNode(node.getRight(), element);
+        }
+
+        return containsNode(node.getLeft(), element);
     }
 
     public BinarySearchTree<E> search(E element) {
         return null;
     }
+
     public List<E> range(E first, E second) {
-      return null;
+        return null;
     }
+
     public void deleteMin() {
 
     }
+
     public void deleteMax() {
 
     }
@@ -101,6 +142,7 @@ public class BinarySearchTree<E extends Comparable<E>> {
     private boolean isLess(E element, Node<E> node) {
         return element.compareTo(node.getValue()) < 0;
     }
+
     private boolean isGreater(E element, Node<E> node) {
         return element.compareTo(node.getValue()) > 0;
     }
