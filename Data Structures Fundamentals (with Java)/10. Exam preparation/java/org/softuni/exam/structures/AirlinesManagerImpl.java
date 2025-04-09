b6ef8080eadc884812d517c5ae74f038d5dd8f8b
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2025-04-09 22:32:16 +0300

    1. Delivery System / 2.Airlines

--- Data Structures Fundamentals (with Java)/10. Exam preparation/java/org/softuni/exam/structures/AirlinesManagerImpl.java
+++ Data Structures Fundamentals (with Java)/10. Exam preparation/java/org/softuni/exam/structures/AirlinesManagerImpl.java
@@ -3,45 +3,84 @@ package org.softuni.exam.structures;
 import org.softuni.exam.entities.Airline;
 import org.softuni.exam.entities.Flight;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class AirlinesManagerImpl implements AirlinesManager {
+
+    Map<String, Airline> airlines = new LinkedHashMap<>();
+    Map<String, Flight> flights = new LinkedHashMap<>();
+    Map<String, Flight> completedFlights = new LinkedHashMap<>();
+    Map<String, List<Flight>> flightsByAirline = new LinkedHashMap<>();
+
     @Override
     public void addAirline(Airline airline) {
-
+        airlines.put(airline.getId(), airline);
+        flightsByAirline.put(airline.getId(), new ArrayList<>());
     }
 
     @Override
     public void addFlight(Airline airline, Flight flight) {
+        if (!contains(airline)) {
+            throw new IllegalArgumentException();
+        }
+
+        flights.put(flight.getId(), flight);
+        List<Flight> currentAirlineFlights = flightsByAirline.get(airline.getId());
+        currentAirlineFlights.add(flight);
+
 
     }
 
     @Override
     public boolean contains(Airline airline) {
-        return false;
+        return airlines.containsKey(airline.getId());
     }
 
     @Override
     public boolean contains(Flight flight) {
-        return false;
+        return flights.containsKey(flight.getId());
     }
 
     @Override
     public void deleteAirline(Airline airline) throws IllegalArgumentException {
-
+        if (!contains(airline)) {
+            throw new IllegalArgumentException();
+        }
+        airlines.remove(airline.getId());
+        List<Flight> removedAirlineFlights = flightsByAirline.get(airline.getId());
+        for (Flight flight : removedAirlineFlights) {
+            flights.remove(flight.getId());
+        }
     }
 
     @Override
     public Iterable<Flight> getAllFlights() {
-        return null;
+        return flights.values();
     }
 
     @Override
     public Flight performFlight(Airline airline, Flight flight) throws IllegalArgumentException {
-        return null;
+
+        if (!contains(airline) || !contains(flight)) {
+            throw new IllegalArgumentException();
+        }
+
+        Flight completedFlight = flights.get(flight.getId());
+        completedFlight.setCompleted(true);
+
+        return completedFlight;
     }
 
     @Override
     public Iterable<Flight> getCompletedFlights() {
-        return null;
+        return flights.values()
+                .stream()
+                .filter(Flight::isCompleted)
+                .collect(Collectors.toList());
     }
 
     @Override
--- Data Structures Fundamentals (with Java)/10. Exam preparation/java/org/softuni/exam/structures/DeliveriesManagerImpl.java
+++ Data Structures Fundamentals (with Java)/10. Exam preparation/java/org/softuni/exam/structures/DeliveriesManagerImpl.java
@@ -3,24 +3,29 @@ package org.softuni.exam.structures;
 import org.softuni.exam.entities.Deliverer;
 import org.softuni.exam.entities.Package;
 
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class DeliveriesManagerImpl implements DeliveriesManager {
 
     private Map<String, Deliverer> deliverersById = new LinkedHashMap<>();
     private Map<String, Package> packagesById = new LinkedHashMap<>();
+    private Map<String, Package> unassingnedPackages = new LinkedHashMap<>();
+
+    private Map<String, Integer> packagesCountByDeliverer = new LinkedHashMap<>();
 
     @Override
     public void addDeliverer(Deliverer deliverer) {
         deliverersById.put(deliverer.getId(), deliverer);
+        packagesCountByDeliverer.put(deliverer.getId(), 0);
     }
 
     @Override
     public void addPackage(Package _package) {
         packagesById.put(_package.getId(), _package);
+        unassingnedPackages.put(_package.getId(), _package);
     }
 
     @Override
@@ -48,21 +53,36 @@ public class DeliveriesManagerImpl implements DeliveriesManager {
 
     @Override
     public void assignPackage(Deliverer deliverer, Package _package) throws IllegalArgumentException {
+        if (!contains(deliverer) || !contains(_package)) {
+            throw new IllegalArgumentException();
+        }
 
+        Integer currentCount = packagesCountByDeliverer.get(deliverer.getId());
+        packagesCountByDeliverer.put(deliverer.getId(), currentCount + 1);
+        unassingnedPackages.remove(_package.getId());
     }
 
     @Override
     public Iterable<Package> getUnassignedPackages() {
-        return null;
+        return unassingnedPackages.values();
     }
 
     @Override
     public Iterable<Package> getPackagesOrderedByWeightThenByReceiver() {
-        return null;
+        return packagesById.values()
+                .stream()
+                .sorted(Comparator.comparing(Package::getWeight).reversed()
+                        .thenComparing(Package::getReceiver))
+                .collect(Collectors.toList());
     }
 
     @Override
     public Iterable<Deliverer> getDeliverersOrderedByCountOfPackagesThenByName() {
-        return null;
+        return deliverersById.values()
+                .stream()
+                .sorted(Comparator.comparing((Deliverer d) -> packagesCountByDeliverer.get(d.getId()))
+                        .reversed()
+                        .thenComparing(Deliverer::getName))
+                .collect(Collectors.toList());
     }
 }
