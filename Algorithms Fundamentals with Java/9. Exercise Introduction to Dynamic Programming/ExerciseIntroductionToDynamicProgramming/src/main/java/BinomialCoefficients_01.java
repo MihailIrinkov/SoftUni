238c289d527c0a48b5025e447c732ce61a832e3f
MihailIrinkov <106864479+MihailIrinkov@users.noreply.github.com>
2025-06-18 16:45:57 +0300

    task 1

---
+++ Algorithms Fundamentals with Java/9. Exercise Introduction to Dynamic Programming/ExerciseIntroductionToDynamicProgramming/src/main/java/BinomialCoefficients_01.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class BinomialCoefficients_01 {
+
+    public static long memory[][];
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        int k = Integer.parseInt(scanner.nextLine());
+
+        memory = new long[n + 1][k + 1];
+
+        long binom = calcBinom(n, k);
+
+        System.out.println(binom);
+    }
+
+    private static long calcBinom(int n, int k) {
+        if (k == 0 || k == n) {
+            return 1;
+        }
+
+        if (memory[n][k]!= 0) {
+            return memory[n][k];
+        }
+
+        return memory[n][k] = calcBinom(n -1, k) + calcBinom(n - 1, k - 1);
+    }
+}
